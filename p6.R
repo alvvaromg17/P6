b80eab1c1fbbe0e9b9cd4ce633525088b93aa7b0
alvvaromg17 <alvvaromg17@gmail.com>
2023-03-13 16:12:59 +0100

    a

--- p6.R
+++ p6.R
@@ -1,1 +1,19 @@
-a
+install.packages("tinytex")
+library (tinytex)
+
+rnorm (5)
+rnorm (5)
+rnorm (5)
+
+set.seed(1)
+rnorm (5)
+
+set.seed(1)
+rnorm (10)
+
+set.seed (2)
+rnorm (5)
+set.seed (2)
+rnorm (2)
+
+rtools
